aab234eabc71c1b81d912ddfa0f178aae2abd18c
Elijah Gutman <gutmane7@gmail.com>
2025-01-20 20:32:49 -0500

    keep the gists alive

---
+++ Gisthere.jsx
@@ -0,0 +1,13 @@
+I am now using this as a place to keep track of my IT thougths even if they have nothing specifically to do
+with my OWN projects and web development. 
+
+I learned how to use Jira the other day, I already used Jira...., its called gitHub projects. People really need to
+relax when it comes to knowing specific types of softwares. Now Tableau on the other hand. That is super useful and to 
+me something unique. The idea of representing my data so easily is a godsend and I look forward to playing with it
+more down the line when we need to bring larger peieces of data to investors. I know how to look up SQL querries but
+to never have to do it slowly like that for a job sounds pretty boss.... 
+
+WE had a developer meeting tonight and disscussed next steps. Long story short we are fleshing out the final parts of 
+landing page, we are creating the sign up and sign in features and populating them with user roles from the database. 
+We are also fixing up the platform(Dashboard) that is seen by the users ONCE they sign in, we just need to flesh
+out some requirements for the schema. 
